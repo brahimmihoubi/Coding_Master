bc71a6504e14dde97f2fbfaca7a6c82aa68dd317
brahimmihoubi <brahimmihoubi357@gmail.com>
2024-03-04 16:02:32 +0000

    brahim

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:aghwat/constants.dart/color.dart';
 import 'package:aghwat/pages/dashboard.dart';
 import 'package:aghwat/pages/notedebac.dart';
-import 'package:flutter/material.dart';
-import 'package:animate_do/animate_do.dart';
 import 'package:aghwat/pages/forgottenpassword.dart';
+import 'package:animate_do/animate_do.dart';
 
 void main() => runApp(const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    ));
+  debugShowCheckedModeBanner: false,
+  home: HomePage(),
+));
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -18,6 +20,31 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  Future<void> loginUser() async {
+    final url = Uri.parse('https://progres.mesrs.dz/api/authentification/v1');
+    final response = await http.post(
+      url,
+      body: jsonEncode({
+        'username': usernameController.text,
+        'password': passwordController.text,
+      }),
+      headers: {'Content-Type': 'application/json'},
+    );
+    if (response.statusCode == 200) {
+      // Successful login, navigate to dashboard
+      // ignore: use_build_context_synchronously
+      Navigator.of(context).push(MaterialPageRoute(builder: (context) => const MyDashboard()));
+    } else {
+      // Handle error
+      // ignore: avoid_print
+      print('Failed to login: ${response.reasonPhrase}');
+      // You can display an error message here
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,9 +54,7 @@ class _HomePageState extends State<HomePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            const SizedBox(
-              height: 80,
-            ),
+            const SizedBox(height: 80),
             Padding(
               padding: const EdgeInsets.all(20),
               child: SingleChildScrollView(
@@ -37,25 +62,24 @@ class _HomePageState extends State<HomePage> {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
                     FadeInUp(
-                        duration: const Duration(milliseconds: 1000),
-                        child: const Center(
-                            child: Text(
+                      duration: const Duration(milliseconds: 1000),
+                      child: const Center(
+                        child: Text(
                           "Login",
-                          style: TextStyle(
-                              color: AppColors.PRIMARY_COLOR, fontSize: 40),
-                        ))),
-                    const SizedBox(
-                      height: 10,
+                          style: TextStyle(color: AppColors.PRIMARY_COLOR, fontSize: 40),
+                        ),
+                      ),
                     ),
+                    const SizedBox(height: 10),
                     FadeInUp(
-                        duration: const Duration(milliseconds: 1300),
-                        child: const Center(
-                          child: Text(
-                            "Welcome Back",
-                            style: TextStyle(
-                                color: AppColors.PRIMARY_COLOR, fontSize: 18),
-                          ),
-                        )),
+                      duration: const Duration(milliseconds: 1300),
+                      child: const Center(
+                        child: Text(
+                          "Welcome Back",
+                          style: TextStyle(color: AppColors.PRIMARY_COLOR, fontSize: 18),
+                        ),
+                      ),
+                    ),
                   ],
                 ),
               ),
@@ -64,81 +88,80 @@ class _HomePageState extends State<HomePage> {
             Expanded(
               child: Container(
                 decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(60),
-                        topRight: Radius.circular(60))),
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(60), topRight: Radius.circular(60)),
+                ),
                 child: Padding(
                   padding: const EdgeInsets.all(30),
                   child: SingleChildScrollView(
                     child: Column(
                       children: <Widget>[
-                        const SizedBox(
-                          height: 60,
-                        ),
+                        const SizedBox(height: 60),
                         FadeInUp(
-                            duration: const Duration(milliseconds: 1400),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(10),
-                                  boxShadow: const [
-                                    BoxShadow(
-                                        color: Color.fromARGB(38, 68, 137, 255),
-                                        blurRadius: 20,
-                                        offset: Offset(0, 10))
-                                  ]),
-                              child: Column(
-                                children: <Widget>[
-                                  Container(
-                                      padding: const EdgeInsets.all(10),
-                                      decoration: const BoxDecoration(
-                                        color: Colors.white,
-                                        borderRadius: BorderRadius.only(
-                                          topLeft: Radius.circular(50),
-                                          topRight: Radius.circular(50),
-                                        ),
-                                      ),
-                                      child: const TextField(
-                                        decoration: InputDecoration(
-                                            prefixIcon: Icon(Icons.person),
-                                            hintText: "Username",
-                                            hintStyle: TextStyle(
-                                                color: AppColors.TEXT_COLOR),
-                                            border: InputBorder.none),
-                                      )),
-                                  Container(
-                                    padding: const EdgeInsets.all(10),
-                                    decoration: const BoxDecoration(
-                                      color: AppColors.PRIMARY_TEXTE_COLOR,
-                                      borderRadius: BorderRadius.only(
-                                        topLeft: Radius.circular(30),
-                                        topRight: Radius.circular(30),
-                                      ),
-                                    ),
-                                    child: const TextField(
-                                      obscureText: true,
-                                      decoration: InputDecoration(
-                                          prefixIcon: Icon(Icons.key),
-                                          hintText: "Password",
-                                          hintStyle: TextStyle(
-                                              color: AppColors.TEXT_COLOR),
-                                          border: InputBorder.none),
+                          duration: const Duration(milliseconds: 1400),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                              boxShadow: const [
+                                BoxShadow(
+                                  color: Color.fromARGB(38, 68, 137, 255),
+                                  blurRadius: 20,
+                                  offset: Offset(0, 10),
+                                )
+                              ],
+                            ),
+                            child: Column(
+                              children: <Widget>[
+                                Container(
+                                  padding: const EdgeInsets.all(10),
+                                  decoration: const BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.only(
+                                      topLeft: Radius.circular(50),
+                                      topRight: Radius.circular(50),
                                     ),
                                   ),
-                                ],
-                              ),
-                            )),
-                        const SizedBox(
-                          height: 40,
+                                  child: TextField(
+                                    controller: usernameController,
+                                    decoration: const InputDecoration(
+                                      prefixIcon: Icon(Icons.person),
+                                      hintText: "Username",
+                                      hintStyle: TextStyle(color: AppColors.TEXT_COLOR),
+                                      border: InputBorder.none,
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  padding: const EdgeInsets.all(10),
+                                  decoration: const BoxDecoration(
+                                    color: AppColors.PRIMARY_TEXTE_COLOR,
+                                    borderRadius: BorderRadius.only(
+                                      topLeft: Radius.circular(30),
+                                      topRight: Radius.circular(30),
+                                    ),
+                                  ),
+                                  child: TextField(
+                                    controller: passwordController,
+                                    obscureText: true,
+                                    decoration: const InputDecoration(
+                                      prefixIcon: Icon(Icons.key),
+                                      hintText: "Password",
+                                      hintStyle: TextStyle(color: AppColors.TEXT_COLOR),
+                                      border: InputBorder.none,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
                         ),
+                        const SizedBox(height: 40),
                         FadeInUp(
                           duration: const Duration(milliseconds: 1500),
                           child: ListTile(
                             onTap: () {
-                              Navigator.of(context).push(MaterialPageRoute(
-                                  builder: (context) =>
-                                      const ForgottenPassword()));
+                              Navigator.of(context).push(MaterialPageRoute(builder: (context) => const ForgottenPassword()));
                             },
                             title: const Text(
                               "Forgot Password?",
@@ -146,64 +169,47 @@ class _HomePageState extends State<HomePage> {
                             ),
                           ),
                         ),
-                        const SizedBox(
-                          height: 40,
-                        ),
+                        const SizedBox(height: 40),
                         FadeInUp(
                           duration: const Duration(milliseconds: 1600),
                           child: MaterialButton(
-                              onPressed: () {
-                                Navigator.of(context).push(
-                                  MaterialPageRoute(
-                                      builder: (context) =>
-                                          const MyDashboard()),
-                                );
-                              },
-                              height: 50,
-                              color: AppColors.BUTTON_COLOR,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(50),
-                              ),
-                              child: const Center(
-                                child: ListTile(
-                                  title: Text(
-                                    "                  "
-                                    "Login",
-                                    style: TextStyle(
-                                        color: AppColors.PRIMARY_COLOR,
-                                        fontWeight: FontWeight.bold),
-                                  ),
+                            onPressed: loginUser,
+                            height: 50,
+                            color: AppColors.BUTTON_COLOR,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                            child: const Center(
+                              child: ListTile(
+                                title: Text(
+                                  "Login",
+                                  style: TextStyle(color: AppColors.PRIMARY_COLOR, fontWeight: FontWeight.bold),
                                 ),
-                              )),
-                        ),const SizedBox(
-                          height: 20,
+                              ),
+                            ),
+                          ),
                         ),
+                        const SizedBox(height: 20),
                         FadeInUp(
                           duration: const Duration(milliseconds: 1600),
                           child: MaterialButton(
-                              onPressed: () {
-                                Navigator.of(context).push(
-                                  MaterialPageRoute(
-                                      builder: (context) =>
-                                          const NoteDeBac()),
-                                );
-                              },
-                              height: 50,
-                              color: AppColors.BUTTON_COLOR,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(50),
-                              ),
-                              child: const Center(
-                                child: ListTile(
-                                  title: Text(
-                                    "         "
-                                    "Les notes de bac",
-                                    style: TextStyle(
-                                        color: AppColors.PRIMARY_COLOR,
-                                        fontWeight: FontWeight.bold),
-                                  ),
+                            onPressed: () {
+                              Navigator.of(context).push(MaterialPageRoute(builder: (context) => const NoteDeBac()));
+                            },
+                            height: 50,
+                            color: AppColors.BUTTON_COLOR,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                            child: const Center(
+                              child: ListTile(
+                                title: Text(
+                                  "Les notes de bac",
+                                  style: TextStyle(color: AppColors.PRIMARY_COLOR, fontWeight: FontWeight.bold),
                                 ),
-                              )),
+                              ),
+                            ),
+                          ),
                         ),
                       ],
                     ),
