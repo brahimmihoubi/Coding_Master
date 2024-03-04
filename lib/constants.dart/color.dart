31c95ff7a0bc3531ba70941402efbf11b298eef8
brahimmihoubi <brahimmihoubi357@gmail.com>
2024-03-04 09:50:35 +0000

    brahim

---
+++ lib/constants.dart/color.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color PRIMARY_COLOR = Colors.blueGrey;
+  static const Color PRIMARY_TEXTE_COLOR = Colors.white;
+  // ignore: non_constant_identifier_names
+  static  Color BUTTON_COLOR = Colors.blue.shade900;
+  static const Color TEXT_COLOR = Colors.grey;
+}
+
+final myGradient = LinearGradient(begin: Alignment.topCenter, colors: [
+  Colors.blue.shade900,
+  Colors.blue.shade800,
+  Colors.blue.shade400,
+]);
+
+
